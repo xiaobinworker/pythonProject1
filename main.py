122e59f4ab5f5ac7dd2f9010139eac59f7bd3215
qiyb <qiyb@briup.com>
2022-04-11 08:38:30 +0800

    conflict_version2

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("first pycharm")
     print("hot-fix version")
+    print("hotfix_conflict")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
